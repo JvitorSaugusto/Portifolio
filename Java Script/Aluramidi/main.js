f694d59d2aa2486a32d5c2734df044052c2f0ee5
João Vitor Santos Augusto <jvitorsaugusto@outlook.com>
2023-04-25 20:59:35 -0300

    Update main.js

--- Java Script/Aluramidi/main.js
+++ Java Script/Aluramidi/main.js
@@ -6,6 +6,19 @@ function tocaSomPom(){
     document.querySelector('#som_tecla_pom').play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+/*document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
-//quando o onclick deste elemento da tecla pom seja executado, queremos que chame a função tocaSomPom;.
+quando o onclick deste elemento da tecla pom seja executado, queremos que chame a função tocaSomPom;.
+
+Utilizamos o document.querySelectorAll() para selecionarmos todos os elementos do seletor desejado, reduzindo a quantidade de repetição de código, que consequentemente, promoverá uma otimização no código em alternativa a repetição.
+
+Ele retorna uma lista (NodeList) com todos os elementos do seletor informado evitando a repetição de código.
+
+As listas se iniciam em 0 e vão até o valor total de itens -1 utilizando do simbolo []
+*/
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+listaDeTeclas[0];
+
+listaDeTeclas[0].onclick = tocaSomPom;
