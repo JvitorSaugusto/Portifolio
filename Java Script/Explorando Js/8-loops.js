1a0517d593e95c4f75cbbc50ce5f5ee818c110a2
João Vitor Santos Augusto <jvitorsaugusto@outlook.com>
2023-05-17 20:13:01 -0300

    Att

--- Java Script/Explorando Js/8-loops.js
+++ Java Script/Explorando Js/8-loops.js
@@ -18,13 +18,37 @@ console.log(listaDeDestinos);
 const podeComprar = idadeComprador >= 18 || estaAcompanhada == true;
 
 let contador = 0;
+let destinoExiste = false;
+
 while(contador <3){       // Enquanto = While
     if(listaDeDestinos [contador] == destino){
-        console.log("Destino existe");
-    }else{
-        console.log("Destino não existe");
+        destinoExiste = true;
+        break; //interrompe o código fazendo que a execução pule 1 laço, neste caso o código ira pular o "contador +=1;" e irá direto para  o "console.log("Destino existe: ", destinoExiste);"
     }
     contador +=1;
 }
 
-console.log(listaDeDestinos);
+console.log("Destino existe: ", destinoExiste);
+
+if (podeComprar && destinoExiste){
+    console.log("Boa viagem");
+}else{
+    console.log("Desculpe tivemos um erro!");
+}
+
+/*exemplo utilizando for - O for possui uma organização de execução que pode soar estranha: a inicialização, a condicional e o incremento. No momento em que a condição que inserimos não for mais verdadeira o looping é encerrado.
+
+for(let cont = 0 ; cont < 3 ; cont++){
+    if(listaDeDestinos [contador] == destino){
+        destinoExiste = true;
+}
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+O for é bastante popular e comum no universo da programação, inclusive, utilizamos a variável cont, mas o mais comum para este tipo de operação é utilizar o nome i como auxiliar do for:
+
+for(let i = 0 ; i <3 ; i++){
+    if(listaDeDestinos[contador] == destino){
+        destinoExiste = true;
+        }
+}
+
+*/
