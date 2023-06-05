9a0c5f43d4c8d6183e2dd9495a8e7e54ff2ee363
João Vitor Santos Augusto <jvitorsaugusto@outlook.com>
2023-06-05 17:46:29 -0300

    Update main.js

--- Java Script/-robotron-2000-projeto_inicial/js/main.js
+++ Java Script/-robotron-2000-projeto_inicial/js/main.js
@@ -5,15 +5,13 @@ const braco= document.querySelector("#braco");
 const controle = document.querySelectorAll(".controle-ajuste");
 
 controle.forEach( (elemento)=> {
-    console.log(elemento)
-})
-
-somar.addEventListener("click", ()=> {manipulaDados("somar")});
-
-subtrair.addEventListener("click",()=> {manipulaDados("subtrair")});
+    elemento.addEventListener("click",(evento) => {
+        manipulaDados("evento.target.text.Content")
+    })
+});
 
 function manipulaDados(operacao) {
-    if(operacao === "subtrair") {
+    if(operacao === "-") {
         braco.value = parseInt(braco.value) - 1
     } else {
         braco.value = parseInt(braco.value) + 1
