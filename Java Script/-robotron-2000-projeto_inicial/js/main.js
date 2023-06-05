bae2d4ba638490b655c75d176ec97ac1c4d9f7bb
João Vitor Santos Augusto <jvitorsaugusto@outlook.com>
2023-06-05 17:37:28 -0300

    Att

--- Java Script/-robotron-2000-projeto_inicial/js/main.js
+++ Java Script/-robotron-2000-projeto_inicial/js/main.js
@@ -1,9 +1,29 @@
-const robotron = document.querySelector("#Robotron");
+const subtrair = document.querySelector("#subtrair");
+const somar= document.querySelector("#somar");
+const braco= document.querySelector("#braco");
 
-robotron.addEventListener("click", dizOi); //adiciona um evento (clique) - addEventListener
+const controle = document.querySelectorAll(".controle-ajuste");
 
-function dizOi (nome) {
-    console.log("oi" + nome);
-    console.log("Bem vindo ao Robotron 2000")
-} 
-dizOi("Pedro")
+controle.forEach( (elemento)=> {
+    console.log(elemento)
+})
+
+somar.addEventListener("click", ()=> {manipulaDados("somar")});
+
+subtrair.addEventListener("click",()=> {manipulaDados("subtrair")});
+
+function manipulaDados(operacao) {
+    if(operacao === "subtrair") {
+        braco.value = parseInt(braco.value) - 1
+    } else {
+        braco.value = parseInt(braco.value) + 1
+    }
+};
+
+/*
+parseInt transforma uma string numerica em numero inteiro, não converte expressões com casos decimais ouu que não são realmente numeros
+
+Aqui os eventos click - soma - subtrair - Vão avisar a função MANIPULADADOS o que ela deve realizar, somar ou diminuir em 1 ao ser clicado.
+
+FOREACH - O forEach() é muito simples de ser utilizado, toda vez ele é usado temos um elemento e um índice. O elemento é aquilo que foi clicado e o índice é o indíce do array. Então controle.forEach() faz algo.
+*/
--- Java Script/TESTE - manipulando obj/main.js
+++ Java Script/TESTE - manipulando obj/main.js
@@ -4,3 +4,17 @@ const resultado = document.querySelector(".resultado");
 elemento.addEventListener("click", (evento) => {   // o parametro evento serve para mostrar detalhadamente as açoes no console. => Função anonima. 
   resultado.innerHTML = "Fui clicado"             //innerHTML - adiciona um texto na tela
 })
+
+
+
+/*
+
+robotron.addEventListener("click", dizOi); //adiciona um evento (clique) - addEventListener
+
+function dizOi (nome) {
+    console.log("oi" + nome);
+    console.log("Bem vindo ao Robotron 2000")
+} 
+dizOi("Pedro")
+
+*/
