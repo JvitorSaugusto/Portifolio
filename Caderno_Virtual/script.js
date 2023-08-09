398eb7ccfb3b0a37f1ed6d23105ca88a1166170b
João Vitor Santos Augusto <jvitorsaugusto@outlook.com>
2023-08-09 12:51:30 -0300

    Update script.js

--- Caderno_Virtual/script.js
+++ Caderno_Virtual/script.js
@@ -20,17 +20,18 @@ tela.height =700;
 const desenharLinha = (linha)=> {
 
 contexto.beginPath(); /*Inicio do desenho/inico do seu caminho*/
-contexto.moveTo(linha.posAnterior.x,linha.posAnterior.y); /*Movimento do seu cursor enquanto desenha */
-contexto.lineTo(linha.pos.x,linha.pos.y) /*Até onde sua linha vai */
+contexto.moveTo(linha.posAnterior.x, linha.posAnterior.y); /*Movimento do seu cursor enquanto desenha */
+contexto.lineTo(linha.pos.x, linha.pos.y) /*Até onde sua linha vai */
 contexto.stroke(); /*Aparecer o desenho */
 }
 
 tela.onmousedown = (evento)=>{pincel.ativo = true}; //pincel ativa ao clicar e segurar
 tela.onmouseup = (evento)=>{pincel.ativo = false}; //desativa ao soltar o mouse
+
 tela.onmousemove = (evento) => {
     pincel.pos.x = evento.clientX
     pincel.pos.y = evento.clientY
-    pincel.movendo = true
+    pincel.movendo = true;
 }
 
 //tela esta na frente do comando pois só queremos q esses eventos  ocorram nela e em mais nada. Evento client.x mapeia a posição em x, assim atualizando o pincel.
@@ -42,22 +43,11 @@ const ciclo = () =>{
     }
     pincel.posAnterior = {x: pincel.pos.x, y: pincel.pos.y}
 
-    setTimeout(ciclo, 5);
+    setTimeout(ciclo, 100);
 
-} //verificando se o pincel esta ativo, se movendo e a posição anterior do mesmo, depois chamando o desenharLinha e passar um obj com pos e posAnterior usando os pinceis, e sempre q parar de mover o pincel, o pincel.movendo vai para false. Toda vez após esse ciclo meu pincel.posAnterior vai receber x e y da posição dos pinceis
+} 
+//verificando se o pincel esta ativo, se movendo e a posição anterior do mesmo, depois chamando o desenharLinha e passar um obj com pos e posAnterior usando os pinceis, e sempre q parar de mover o pincel, o pincel.movendo vai para false. Toda vez após esse ciclo meu pincel.posAnterior vai receber x e y da posição dos pinceis
 
 ciclo ();
-
-
 //desenharLinha({pos: {x:350, y:250},posAnterior: {x:10, y:10}})
-
-
-
-
-
-
-
-
-
-
 })
