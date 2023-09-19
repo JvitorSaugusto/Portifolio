e09fbf3a6d22a62fc7ae8d8a24d939d8c7e37f3d
João Vitor Santos Augusto <jvitorsaugusto@outlook.com>
2023-09-19 12:11:43 -0300

    Create script.js

---
+++ Java Script/udemy/Funções/script.js
@@ -0,0 +1,3 @@
+// Funções são estruturas de código menores, o ideal é que elas só tenham um objeto; JS Já tem funções prontas mas tbm podemos crialas
+
+// Definindo uma função; Primeiramente digite Function > nome > parâmetro que ficam entre () > corpo da função fica entre {}; Geralmente uma função retorna um valor
