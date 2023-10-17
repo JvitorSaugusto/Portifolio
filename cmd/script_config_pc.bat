bc17a0d0ff0ea9556319197875dad09a069dd0ed
João Vitor Santos Augusto <jvitorsaugusto@outlook.com>
2023-10-17 12:54:36 -0300

    Update script_config_pc.bat

--- cmd/script_config_pc.bat
+++ cmd/script_config_pc.bat
@@ -2,7 +2,7 @@
 cls
 :menu
 cls
-color 71
+color 0
 echo Usuario Logado: %username%                            Computador: %computername%
 date /t  
 
@@ -12,7 +12,11 @@ echo  1. CHROME
 echo  2. LIGHT SHOT
 echo  3. DISCORD   
 echo  4. LOGITECH      
-echo  5. SAIR      
+echo  5. BITDEFENDER FREE
+echo  6. WINRAR
+echo  7. TODOS OS APPS .EXE DA PASTA
+echo  8. TODOS OS APPS DA LISTA
+echo  9. SAIR      
 echo __________________________ 
                
 set /p opcao= Escolha uma opcao:
@@ -21,8 +25,11 @@ if "%opcao%" equ "1" goto opcao1
 if "%opcao%" equ "2" goto opcao2
 if "%opcao%" equ "3" goto opcao3
 if "%opcao%" equ "4" goto opcao4
-if "%opcao%" equ "5" goto opcao4
-if %opcao% GEQ 6 goto opcao6
+if "%opcao%" equ "5" goto opcao5
+if "%opcao%" equ "6" goto opcao6
+if "%opcao%" equ "7" goto opcao7
+if "%opcao%" equ "8" goto opcao8
+if %opcao% GEQ 9 goto opcao9
 
 
 :opcao1
@@ -82,9 +89,62 @@ if exist "C:\Users\%username%\Downloads\options_installer.exe" (
 )
 
 :opcao5
-exit
+cls
+if exist "C:\Users\%username%\Downloads\bitdefender_avfree.exe" (
+    start "" "C:\Users\%username%\Downloads\bitdefender_avfree.exe"
+    pause
+    goto menu
+) else (
+    bitsadmin /transfer AcessoRemoto /priority normal https://download.bitdefender.com/windows/installer/en-us/bitdefender_avfree.exe "C:\Users\%username%\Downloads\bitdefender_avfree.exe"
+    pause
+    start "" "C:\Users\%username%\Downloads\bitdefender_avfree.exe"
+    pause
+    goto menu
+)
 
 :opcao6
+cls
+if exist "C:\Users\%username%\Downloads\winrar-x64-624br.exe" (
+    start "" "C:\Users\%username%\Downloads\winrar-x64-624br.exe"
+    pause
+    goto menu
+) else (
+    bitsadmin /transfer AcessoRemoto /priority normal https://www.win-rar.com/fileadmin/winrar-versions/winrar/winrar-x64-624br.exe "C:\Users\%username%\Downloads\winrar-x64-624br.exe"
+    pause
+    start "" "C:\Users\%username%\Downloads\winrar-x64-624br.exe"
+    pause
+    goto menu
+)
+
+:opcao7
+cls
+echo -----------------------------------
+echo Iniciando todos os aplicativos no diretório...
+echo -----------------------------------
+for %%f in ("%~dp0\*.exe") do (
+    start "" "%%f"
+)
+pause
+goto menu
+
+:opcao8
+cls
+echo -----------------------------------
+echo Iniciando todos os aplicativos...
+echo -----------------------------------
+start "" "%~dp0\ChromeSetup.exe"
+start "" "%~dp0\setup-lightshot.exe"
+start "" "%~dp0\DiscordSetup.exe"
+start "" "%~dp0\options_installer.exe"
+start "" "%~dp0\bitdefender_avfree.exe"
+start "" "%~dp0\winrar-x64-624br.exe"
+pause
+goto menu
+
+:opcao9
+exit
+
+:opcao10
 cls
 echo -----------------------------------
 echo Opcao invalida! Escolha outra opcao
